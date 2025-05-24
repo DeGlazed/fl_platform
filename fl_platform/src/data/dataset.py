d16b0f4f2d2157acf38c6cb35174de5421e884c6
DeGlazed <raducutanase@gmail.com>
2025-05-24 11:10:38 +0300

    Add dataset implemntations for tdrive dataset

--- fl_platform/src/data/dataset.py
+++ fl_platform/src/data/dataset.py
@@ -275,41 +275,264 @@ def get_client_quality_statistics(partition_id, num_partitions, label_mapping, d
         "sampling_regularity_std": 1/(np.median(sampling_regularity_stds) + 1e-8)
     }
 
+class TDriveTrajectorySeqToSeqDataset(Dataset):
+    def __init__(self, data_dict, clients_subset,
+                 feature_extractor=None, min_length=10, sequence_length=5, prediction_length=5):
+        self.samples = []
+        self.feature_extractor = feature_extractor or self.default_data_extractor
+        self.min_length = min_length
+        self.sequence_length = sequence_length
+        self.prediction_length = prediction_length
+
+        for client_id in clients_subset:
+            user_data = data_dict.get(client_id, {})
+            for label_key, df in user_data.items():
+                if len(df) < min_length:
+                    continue
+
+                features = self.feature_extractor(df)
+                
+                for i in range(len(features) - sequence_length - prediction_length + 1):
+                    input_sequence = features[i:i+sequence_length]
+                    target_sequence = features[i+sequence_length:i+sequence_length+prediction_length][:, :2]
+                    self.samples.append((input_sequence, target_sequence))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        input_seq, target_seq = self.samples[idx]
+        return torch.tensor(input_seq, dtype=torch.float32), torch.tensor(target_seq, dtype=torch.float32)
+
+    @staticmethod
+    def default_data_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        timestamps = timestamps.reshape(-1, 1)
+
+        features = np.hstack([coords, timestamps])
+        return features
+
+    @staticmethod
+    def location_time_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        timestamps = timestamps - timestamps[0]
+        timestamps = timestamps.reshape(-1, 1)
+
+        features = np.hstack([coords, timestamps])
+        return features
+    
+class TDriveTrajectoryNextPointDataset(Dataset):
+    def __init__(self, data_dict, clients_subset,
+                 feature_extractor=None, min_length=10, sequence_length=20):
+        self.samples = []
+        self.feature_extractor = feature_extractor or self.default_data_extractor
+        self.min_length = min_length
+        self.sequence_length = sequence_length
+
+        for client_id in clients_subset:
+            user_data = data_dict.get(client_id, {})
+            for label_key, df in user_data.items():
+                if len(df) < min_length:
+                    continue
+
+                features = self.feature_extractor(df)
+                
+                for i in range(len(features) - sequence_length):
+                    input_sequence = features[i:i+sequence_length]
+                    target_point = features[i+sequence_length][:2]
+                    self.samples.append((input_sequence, target_point))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        sequence, target = self.samples[idx]
+        return torch.tensor(sequence, dtype=torch.float32), torch.tensor(target, dtype=torch.float32)
+
+    @staticmethod
+    def default_data_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        timestamps = timestamps.reshape(-1, 1)
+
+        features = np.hstack([coords, timestamps])
+        return features
+
+    @staticmethod
+    def location_time_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        timestamps = timestamps - timestamps[0]
+        timestamps = timestamps.reshape(-1, 1)
+
+        features = np.hstack([coords, timestamps])
+        return features
+    
+class TDriveMobilityDataset(Dataset):
+    def __init__(self, data_dict, clients_subset, label_mapping,
+                 feature_extractor=None, min_length=10):
+        self.samples = []
+        self.label_mapping = label_mapping
+        self.feature_extractor = feature_extractor or self.default_data_extractor
+        self.min_length = min_length
+
+        for client_id in clients_subset:
+            user_data = data_dict.get(client_id, {})
+            for label_key, df in user_data.items():
+                if len(df) < min_length:
+                    continue
+
+                # Extract label
+                label = 'taxi'
+                label_id = self.label_mapping[label]
+
+                features = self.feature_extractor(df)
+                self.samples.append((features, label_id))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        features, label = self.samples[idx]
+        return torch.tensor(features, dtype=torch.float32), torch.tensor(label, dtype=torch.long)
+
+    @staticmethod
+    def default_data_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        timestamps = timestamps.reshape(-1, 1)
+
+        features = np.hstack([coords, timestamps])
+        return features
+
+    @staticmethod
+    def location_time_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        # Normalized timestamp
+        timestamps = timestamps - timestamps[0]
+        timestamps = timestamps.reshape(-1, 1)
+
+        features = np.hstack([coords, timestamps])
+        return features
+
+    @staticmethod
+    def delta_extractor(df):
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        lat_lon_deltas = np.diff(coords, axis=0, prepend=coords[0:1])
+        time_deltas = np.diff(timestamps, prepend=timestamps[0]).reshape(-1, 1)
+
+        features = np.hstack([lat_lon_deltas, time_deltas])
+        return features
+    
+    @staticmethod
+    def rich_extractor(df):
+        def compute_harvestine_distance(coord1, coord2) :
+            #need lat lon in radians
+            lat1, lon1 = np.radians(coord1)
+            lat2, lon2 = np.radians(coord2)
+            tmp = np.sin((lat1 - lat2) / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin((lon1 - lon2) / 2) ** 2
+            ang_distance = 2 * np.arcsin(np.sqrt(tmp))
+            return ang_distance * 6371 * 1000
+        
+        def compute_bearing(coord1, coord2) :
+            #need lat lon in radians
+            lat1, lon1 = np.radians(coord1)
+            lat2, lon2 = np.radians(coord2)
+            x = np.cos(lat2) * np.sin(lon2 - lon1)
+            y = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(lon2 - lon1)
+            bearing = np.arctan2(x, y)
+            return np.degrees(bearing) % 360
+        
+        coords = df[['latitude', 'longitude']].values
+        timestamps = df['timestamp'].astype('float').values
+
+        time_deltas = np.diff(timestamps, prepend=timestamps[0])
+        time_deltas[time_deltas == 0] = 1e-6
+
+        distances = np.array([compute_harvestine_distance(coords[i-1], coords[i]) if i > 0 else 0 for i in range(len(coords))])
+        speeds = distances / time_deltas
+        accelerations = np.diff(speeds, prepend=speeds[0]) / time_deltas
+
+
+        bearings = np.array([compute_bearing(coords[i-1], coords[i]) if i > 0 else 0 for i in range(len(coords))])
+        bearings_delta = np.diff(bearings, prepend=bearings[0])
+        bearing_rates = bearings_delta / time_deltas
+        
+        return np.stack([speeds, accelerations, bearings, bearings_delta, bearing_rates], axis=1)
+    
+        
 if __name__ == "__main__":
-
-    # with open('fl_platform\src\data\processed\geolife_processed_data.pkl', 'rb') as f:
-    #     geo_dataset = pickle.load(f)
-
-    with open('fl_platform\src\data\processed\geolife_next_point_separated_routes.pkl', 'rb') as f:
-        geo_dataset = pickle.load(f)
+  
+    # data = "geolife"
+    data = "tdirve"
     
-    filter_geo_dataset = {}
-    for client_id, data in geo_dataset.items():
-        filtered_data_dict = {}
-        for lable, df in data.items():
-            if 'run' not in lable and 'motorcycle' not in lable:
-                filtered_data_dict[lable] = df
-        filter_geo_dataset[client_id] = filtered_data_dict
-    geo_dataset = filter_geo_dataset
-                    
-    labels = ['walk', 'bus', 'car', 'taxi', 'subway', 'train', 'bike'] #removed 'run' and 'motorcycle'
-    sorted_labels = sorted(labels)
-    label_mapping = {label: idx for idx, label in enumerate(sorted_labels)}
-    # print("Label Mapping:", label_mapping)
+    if data == "geolife":
+        # with open('fl_platform\src\data\processed\geolife_processed_data.pkl', 'rb') as f:
+        #     geo_dataset = pickle.load(f)
 
-    clients_subset=range(1,65)
-    
-    # dataset = GeoLifeMobilityDataset(geo_dataset, clients_subset, label_mapping,
-    #     feature_extractor=GeoLifeMobilityDataset.rich_extractor
-    # )
+        with open('fl_platform\src\data\processed\geolife_next_point_separated_routes.pkl', 'rb') as f:
+            geo_dataset = pickle.load(f)
+        
+        filter_geo_dataset = {}
+        for client_id, data in geo_dataset.items():
+            filtered_data_dict = {}
+            for lable, df in data.items():
+                if 'run' not in lable and 'motorcycle' not in lable:
+                    filtered_data_dict[lable] = df
+            filter_geo_dataset[client_id] = filtered_data_dict
+        geo_dataset = filter_geo_dataset
+                        
+        labels = ['walk', 'bus', 'car', 'taxi', 'subway', 'train', 'bike'] #removed 'run' and 'motorcycle'
+        sorted_labels = sorted(labels)
+        label_mapping = {label: idx for idx, label in enumerate(sorted_labels)}
+        # print("Label Mapping:", label_mapping)
 
-    dataset = GeoLifeTrajectoryNextPointDataset(geo_dataset, clients_subset)
+        clients_subset=range(1,65)
+        
+        # dataset = GeoLifeMobilityDataset(geo_dataset, clients_subset, label_mapping,
+        #     feature_extractor=GeoLifeMobilityDataset.rich_extractor
+        # )
 
-    print("Dataset Size:", len(dataset))
-    print("Sample Data:", dataset[0])
-    print("Sample Data Shape:", dataset[0][0].shape)
+        dataset = GeoLifeTrajectoryNextPointDataset(geo_dataset, clients_subset)
 
-    client_dataset = get_client_dataset_split_following_normal_distribution(0, 5, dataset)
-    print("Client Dataset Size:", len(client_dataset))
-    print("Sample Data:", client_dataset[0])
-    print("Sample Data Shape:", client_dataset[0][0].shape)
+        print("Dataset Size:", len(dataset))
+        print("Sample Data:", dataset[0])
+        print("Sample Data Shape:", dataset[0][0].shape)
+
+        client_dataset = get_client_dataset_split_following_normal_distribution(0, 5, dataset)
+        print("Client Dataset Size:", len(client_dataset))
+        print("Sample Data:", client_dataset[0])
+        print("Sample Data Shape:", client_dataset[0][0].shape)
+
+    if data == "tdirve":
+        with open('fl_platform\src\data\processed\\tdrive_next_point_separated_routes_by_day.pkl', 'rb') as f:
+            tdrive_dataset = pickle.load(f)
+
+        labels = ['walk', 'bus', 'car', 'taxi', 'subway', 'train', 'bike']
+        sorted_labels = sorted(labels)
+        label_mapping = {label: idx for idx, label in enumerate(sorted_labels)}
+
+        clients_subset = range(1, 65)
+        # dataset = TDriveTrajectorySeqToSeqDataset(tdrive_dataset, clients_subset)
+        # dataset = TDriveTrajectoryNextPointDataset(tdrive_dataset, clients_subset)
+        dataset = TDriveMobilityDataset(tdrive_dataset, clients_subset, label_mapping)
+
+        print("Dataset Size:", len(dataset))
+        print("Sample Data:", dataset[0])
+        print("Sample Data Shape:", dataset[0][0].shape)
+
+        client_dataset = get_client_dataset_split_following_normal_distribution(0, 5, dataset)
+        print("Client Dataset Size:", len(client_dataset))
+        print("Sample Data:", client_dataset[0])
+        print("Sample Data Shape:", client_dataset[0][0].shape)
