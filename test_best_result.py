27cf37bb8fdeba046fd3e20e091b7587e396bd43
DeGlazed <raducutanase@gmail.com>
2025-06-23 17:04:38 +0300

    extract result matrix

---
+++ test_best_result.py
@@ -0,0 +1,13 @@
+from centralized import load_train_test_data
+from fl_platform.src.models.model import SimpleLSTM
+import torch
+
+_, test_dataloader, dataset = load_train_test_data(0, 1)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+input_size = 5
+hidden_size = 64
+num_layers = 2
+num_classes = len(dataset.label_mapping)
+net = SimpleLSTM(input_size, hidden_size, num_layers, num_classes).to(device)
+net.load_state_dict(torch.load("model_96.pth"))
